e972fe4378b3b70390ab63e992b0b3f8967a48bc
JC Scalabre <jc_scalabre@hotmail.com>
2017-10-06 00:11:05 -0500

    Thursday night JC

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,23 +1,24 @@
-var startdate = "2015-10-3";
-var enddate = "2015-10-4";
+var startdate = "2017-09-21";
+var enddate = "2017-09-22";
 
-var datequeryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=date%20between%20%27" + startdate + "%27%20and%20%27" + enddate + "%27"
+var datequeryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=date%20between%20%27" + startdate + "%27%20and%20%27" + enddate + "%27" 
+
+
 
 // Crimes where Latitude are between:
-var lowerlat = 41.91673
-var upperlat = 41.91676
-var latqueryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=latitude%20between%20" + lowerlat + "%20and%20" + upperlat;
-console.log("Lat query URL: " + latqueryURL);
-
+var radius = 150
+var latitude = 41.91673
+var longitude = -87.631749
+var locationqueryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=within_circle(location,%20" + latitude + ",%20" + longitude + ",%20" + radius + ")"
+console.log(datequeryURL);
 
 $.ajax({
-	url: latqueryURL,
+	url: datequeryURL,
 	method: "GET"
 }).done(function(response) {
 	console.log(response);
 	for (var i = 0; i < response.length; i++) {
 		console.log ("Crime #: " + i)
-		console.log (response[i].primary_type)
-		console.log (response[i].description);
+		console.log (response[i])
 	}
 })
