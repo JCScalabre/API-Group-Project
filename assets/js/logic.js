bac3d694abaaa8491d38a96977cc64cf5ecc30cc
JC Scalabre <jc_scalabre@hotmail.com>
2017-10-06 11:57:55 -0500

    changes to logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,58 +1,50 @@
-// Crimes by date:
-var startdate = "2017-09-21";
-var enddate = "2017-09-22";
+// // Crimes by date:
+// var startdate = "2017-09-21";
+// var enddate = "2017-09-22";
 
-var datequeryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=date%20between%20%27" + startdate + "%27%20and%20%27" + enddate + "%27" 
+// var datequeryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=date%20between%20%27" + startdate + "%27%20and%20%27" + enddate + "%27" 
 
 // Crimes by location:
 // Radius in meters:
-var radius = 150;
+var radius = 100;
 // Lat and Long:
-var latitude = 41.91673
+var latitude = 41.87073
 var longitude = -87.631749
 
 var locationqueryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=within_circle(location,%20" + latitude + ",%20" + longitude + ",%20" + radius + ")"
-console.log(datequeryURL);
-
-var results = {};
+console.log(locationqueryURL);
 
 // AJAX Request:
 $.ajax({
 	url: locationqueryURL,
 	method: "GET"
 }).done(function(response) {
-	// console.log(response);
-	results = response;
-	for (var i = 0; i < response.length; i++) {
 
-		results[i] = {
-			"Date": response[i].date,
-			"Type": response[i].primary_type,
-			"lat": response[i].latitude, 
-			"lon": response[i].longitude
-		}
+	console.log(response);
+
+	for (var i = 0; i < response.length; i++) {
+		console.log(response[i].year);
+
+		if (response[i].arrest === true ) {
+
 		marker = new google.maps.Marker({
-			position: new google.maps.LatLng(results[i].lat, results[i].lon),
+			position: new google.maps.LatLng(response[i].latitude, response[i].longitude),
 			map: map
 		});
 
 		google.maps.event.addListener(marker, 'click', (function(marker, i) {
 			return function() {
-				infowindow.setContent(results[i].Type);
+				infowindow.setContent(response[i].primary_type);
 				infowindow.open(map, marker);
 			}
 		})(marker, i));
-		// console.log(results[i]);
+}
 	};
 
-	// console.log(results);
 });
 
-// Map part: ------------------------------------------------------------------------
-
-
 var map = new google.maps.Map(document.getElementById('map'), {
-	zoom: 11,
+	zoom: 12,
 	center: new google.maps.LatLng(41.8781, -87.6298),
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 });
