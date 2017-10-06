5c26229b84c1c91be1ac146bbbe101451aece522
JC Scalabre <jc_scalabre@hotmail.com>
2017-10-06 10:48:19 -0500

    JC

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,10 +4,9 @@ var enddate = "2017-09-22";
 
 var datequeryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=date%20between%20%27" + startdate + "%27%20and%20%27" + enddate + "%27" 
 
-
 // Crimes by location:
 // Radius in meters:
-var radius = 300;
+var radius = 150;
 // Lat and Long:
 var latitude = 41.91673
 var longitude = -87.631749
@@ -15,14 +14,25 @@ var longitude = -87.631749
 var locationqueryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=within_circle(location,%20" + latitude + ",%20" + longitude + ",%20" + radius + ")"
 console.log(datequeryURL);
 
+var object = {};
+
 // AJAX Request:
 $.ajax({
 	url: locationqueryURL,
 	method: "GET"
 }).done(function(response) {
-	console.log(response);
+	// console.log(response);
+	results = response;
 	for (var i = 0; i < response.length; i++) {
-		console.log ("Crime #: " + i)
-		console.log (response[i])
+
+		object[i] = {
+			"Date": response[i].date,
+			"Type": response[i].primary_type,
+			"Latitude": response[i].latitude, 
+			"Longitude": response[i].longitude
+		}
+
+		// console.log(object[i]);
 	};
+	console.log(object);
 });
