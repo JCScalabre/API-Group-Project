d7403fc50e7d9267b93b46afc46655db2205e72a
Nicholas Vree <nicholasvree@Nicholass-MacBook-Air.local>
2017-10-09 10:57:53 -0500

    returnlatlong function NV

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,6 +8,7 @@ var longitude = -87.631749
 var selectedYear;
 var selectedType;
 var gmarkers = [];
+var selectedAddress; 
 
 // Functions: ------------------------------------------------
 
@@ -20,6 +21,8 @@ function removeMarkers(){
 
 // This function performs an AJAX request using just the location URL and then puts all the crimes on the map:
 function locationajax() {
+      returnLatLong()
+
 
     var locationqueryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=within_circle(location,%20" + latitude + ",%20" + longitude + ",%20" + radius + ")"
 
@@ -50,6 +53,9 @@ function locationajax() {
 
 // This function performs an AJAX request using the location + year URL and puts the crimes for that year on the map:
 function locationyearajax() {
+
+          returnLatLong()
+
 
     var locationyearqueryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=within_circle(location,%20" + latitude + ",%20" + longitude + ",%20" + radius + ")&year=" + selectedYear; 
     console.log(locationyearqueryURL);
@@ -321,102 +327,6 @@ var infowindow = new google.maps.InfoWindow();
 
 var marker, i;
 
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
-
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-function initAutocomplete() {
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 41.8781, lng:-87.6298},
-		zoom: 13,
-		mapTypeId: 'roadmap'
-	});
-
-// Create the search box and link it to the UI element.
-var input = document.getElementById('pac-input');
-var searchBox = new google.maps.places.SearchBox(input);
-map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-// Bias the SearchBox results towards current map's viewport.
-map.addListener('bounds_changed', function() {
-	searchBox.setBounds(map.getBounds());
-});
-
-var markers = [];
-
-// Listen for the event fired when the user selects a prediction and retrieve
-// more details for that place.
-searchBox.addListener('places_changed', function() {
-	var places = searchBox.getPlaces();
-
-	if (places.length == 0) {
-		return;
-	}
-
-// Clear out the old markers.
-markers.forEach(function(marker) {
-	marker.setMap(null);
-});
-markers = [];
-
-// For each place, get the icon, name and location.
-var bounds = new google.maps.LatLngBounds();
-places.forEach(function(place) {
-	if (!place.geometry) {
-		console.log("Returned place contains no geometry");
-		return;
-	}
-	var icon = {
-		url: place.icon,
-		size: new google.maps.Size(71, 71),
-		origin: new google.maps.Point(0, 0),
-		anchor: new google.maps.Point(17, 34),
-		scaledSize: new google.maps.Size(25, 25)
-	};
-
-// Create a marker for each place.
-markers.push(new google.maps.Marker({
-	map: map,
-	icon: icon,
-	title: place.name,
-	position: place.geometry.location
-}));
-
-if (place.geometry.viewport) {
-       // Only geocodes have viewport.
-       bounds.union(place.geometry.viewport);
-
-   } else {
-   	bounds.extend(place.geometry.location);
-   }
-});
-map.fitBounds(bounds);
-});
-}
-    //     // Filtering links click handler, it uses the filtering values (data-filterby and data-filtervalue)
-    //     // to filter the markers based on the filter (custom) property set when the marker is created.
-    //     $(document).on('click', '.filters a', function (event) {
-    //         event.preventDefault();
-    //         var $target = $(event.target);
-    //         var type = $target.data('filterby');
-    //         var value = $target.data('filtervalue');
-
-    //         $.each(map.markers, function () {
-    //             if (this.filter[type] == value) {
-    //                 if (this.map == null) {
-    //                     this.setMap(map);
-    //                 }
-    //             } else {
-    //                 this.setMap(null);
-    //             }
-    //         });
-    //     });
-    // };
-
 
 //***//Global Variables - Don't Delete
 
@@ -457,6 +367,39 @@ function buildDropDownOptions(){
         newOption.attr("value", radiusOptions[j]*1609.34)
         $("#radius-dropdown").append(newOption)
     }
+}
+
+
+function returnLatLong(){
+
+    selectedAddress = $("#user-locate").val().trim()
+
+    console.log(selectedAddress)
+
+    var latLongQuery = "https://maps.googleapis.com/maps/api/geocode/json?address=" + selectedAddress + "&key=AIzaSyAeLu14HV0oKo1YYiceQ30EIFOGuBJtvXk"
+
+    console.log("returnran")
+
+    $.ajax({
+        url: latLongQuery,
+        method: "GET"
+    }).done(function(response) {
+
+    console.log(response.results[0].geometry.location.lat);
+    console.log(response.results[0].geometry.location.lng);    
+
+    longitude = response.results[0].geometry.location.lat;
+    latitude = response.results[0].geometry.location.lng;
+
+    console.log(longitude)
+    console.log(latitude)
+
+    
+
+
+
+    });
+
 }
 
 
