af1b604163d0c31d2b480dc3e7278109d16e69cc
JC Scalabre <jc_scalabre@hotmail.com>
2017-10-09 10:11:25 -0500

    Type and Year dropdowns work!

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,16 +1,22 @@
-// // Crimes by date:
-// var startdate = "2017-09-21";
-// var enddate = "2017-09-22";
+// Global variables: ----------------------------------------
 
-// var datequeryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=date%20between%20%27" + startdate + "%27%20and%20%27" + enddate + "%27" 
-
-// Crimes by location:
 // Radius in meters:
 var radius = 200;
 // Lat and Long:
 var latitude = 41.87073
 var longitude = -87.631749
 var selectedYear;
+var selectedType;
+var gmarkers = [];
+
+// Functions: ------------------------------------------------
+
+// This function removes markers on the map:
+function removeMarkers(){
+    for( i = 0; i < gmarkers.length; i++){
+        gmarkers[i].setMap(null);
+    }
+}
 
 // This function performs an AJAX request using just the location URL and then puts all the crimes on the map:
 function locationajax() {
@@ -18,34 +24,35 @@ function locationajax() {
     var locationqueryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=within_circle(location,%20" + latitude + ",%20" + longitude + ",%20" + radius + ")"
 
     $.ajax({
-       url: locationqueryURL,
-       method: "GET"
-   }).done(function(response) {
+        url: locationqueryURL,
+        method: "GET"
+    }).done(function(response) {
 
-       console.log(response);
+        console.log(response);
 
-       for (var i = 0; i < response.length; i++) {
+        for (var i = 0; i < response.length; i++) {
 
-          marker = new google.maps.Marker({
-           position: new google.maps.LatLng(response[i].latitude, response[i].longitude),
-           map: map
-       });
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(response[i].latitude, response[i].longitude),
+                map: map
+            });
+            gmarkers.push(marker);
 
-          google.maps.event.addListener(marker, 'click', (function(marker, i) {
-           return function() {
-            infowindow.setContent("<p id = 'info-window'>"+response[i].primary_type+ "<br>" + moment(response[i].date).format("MMMM D YYYY") +"<br> <a href= 'https://new.tipsubmit.com/#/submit-tip/ChicagoPD' target='_blank'> Submit a tip </a> </p>"  );
-            infowindow.open(map, marker);
-        }
-    })(marker, i));
-      };
-  });
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infowindow.setContent("<p id = 'info-window'>"+response[i].primary_type+ "<br>" + moment(response[i].date).format("MMMM D YYYY") +"<br> <a href= 'https://new.tipsubmit.com/#/submit-tip/ChicagoPD' target='_blank'> Submit a tip </a> </p>"  );
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+        };
+    });
 };
 
 // This function performs an AJAX request using the location + year URL and puts the crimes for that year on the map:
 function locationyearajax() {
 
     var locationyearqueryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=within_circle(location,%20" + latitude + ",%20" + longitude + ",%20" + radius + ")&year=" + selectedYear; 
-
+    console.log(locationyearqueryURL);
     $.ajax({
         url: locationyearqueryURL,
         method: "GET"
@@ -55,10 +62,43 @@ function locationyearajax() {
 
         for (var i = 0; i < response.length; i++) {
 
-            marker = new google.maps.Marker({
+            var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(response[i].latitude, response[i].longitude),
                 map: map
             });
+            gmarkers.push(marker);
+
+
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infowindow.setContent("<p id = 'info-window'>"+response[i].primary_type+ "<br>" + moment(response[i].date).format("MMMM D YYYY") +"<br> <a href= 'https://new.tipsubmit.com/#/submit-tip/ChicagoPD' target='_blank'> Submit a tip </a> </p>"  );
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+        };
+    }); 
+};
+
+// This function performs an AJAX request using the location + crime type URL and puts the crimes for that year on the map:
+function locationtypeajax() {
+
+    var locationtypequeryURL = "https://data.cityofchicago.org/resource/6zsd-86xi.json?$where=within_circle(location,%20" + latitude + ",%20" + longitude + ",%20" + radius + ")&primary_type=" + selectedType; 
+    console.log(locationtypequeryURL);
+    $.ajax({
+        url: locationtypequeryURL,
+        method: "GET"
+    }).done(function(response) {
+
+        console.log(response);
+
+        for (var i = 0; i < response.length; i++) {
+
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(response[i].latitude, response[i].longitude),
+                map: map
+            });
+            gmarkers.push(marker);
+
 
             google.maps.event.addListener(marker, 'click', (function(marker, i) {
                 return function() {
@@ -78,20 +118,27 @@ $("#submit").on("click", function() {
 
 // When the Test button is clicked:
 $("#year2001").on("click", function() {
-    console.log("You clicked the year 2001");
-    year = 2005;
-    locationyearajax();
+    console.log("You clicked the test button");
+    removeMarkers();
 });
 
 // When the user selects a year from the drop down:
 $("#year-dropdown").on("change", function() {
     selectedYear = $("#year-dropdown :selected").attr("value");
     console.log("You changed the year to: " + selectedYear);
+    removeMarkers();
     locationyearajax();
 });
 
-//Places Map with wither proper center, zoom, and style (NV)
+// When the user selects a type from the drop down:
+$("#crime-type-dropdown").on("change", function() {
+    selectedType = $("#crime-type-dropdown :selected").attr("value");
+    console.log("You changed the year to: " + selectedType);
+    removeMarkers();
+    locationtypeajax();
+});
 
+//Places Map with wither proper center, zoom, and style (NV)
 var map = new google.maps.Map(document.getElementById('map'), {
 	zoom: 12,
 	center: new google.maps.LatLng(41.8781, -87.6298),
